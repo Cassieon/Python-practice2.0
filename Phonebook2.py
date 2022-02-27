93b74c35c93e97316e868699a9db41c10488b2ff
Cassieon Craft <cass.m.craft@gmail.com>
2022-02-27 13:38:41 -0500

    added initial entry option

--- Phonebook2.py
+++ Phonebook2.py
@@ -13,10 +13,13 @@ def phonebook():
         3. Delete an entry
         4. List all entries
         5. Quit 
+        
         """)
 phonebook()
 
 while(True):
+    entry = int(input("Select Option:"))
+
     if entry == 1:
        entry = input("What's contact name?")
        if entry in contacts.keys():
@@ -26,3 +29,4 @@ while(True):
         contact_name = input("Enter contacts name:")
         phone_number = input("Enter contacts number:")
         contacts[contact_name] = phone_number
+
