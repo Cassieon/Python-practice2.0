a8f0a630d7caa267416b9a0c3524ad03076165ff
Cassieon Craft <cass.m.craft@gmail.com>
2022-02-27 12:56:01 -0500

    added menu and contacts

---
+++ Phonebook2.py
@@ -0,0 +1,17 @@
+contacts = {
+     "Spongebob": "123-456-7890",
+     "Patrick": "987-654-3210",
+     "Sandy": "192-083-4756",
+    }
+
+def phonebook():
+    print("""
+        Electronic Phone Book
+        **************************
+        1. Look up an entry
+        2. Set an entry
+        3. Delete an entry
+        4. List all entries
+        5. Quit 
+        """)
+phonebook()
